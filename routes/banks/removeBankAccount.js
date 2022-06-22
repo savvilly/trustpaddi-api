8f1edf6b3859d93b79e7d56d61c9a8d04dad83bd
Terry Amagboro <42579208+rukkiecodes@users.noreply.github.com>
2022-06-22 19:17:53 +0000

    delete bank account

--- routes/banks/removeBankAccount.js
+++ routes/banks/removeBankAccount.js
@@ -3,23 +3,23 @@ const checkAuth = require("../../middleware/checkAuth")
 
 const Bank = require("../../models/Banks")
 
-router.post("/removeBank", async (req, res) => {
-  const { bankId } = req.body
+router.post("/removeBank/:bankId", async(req, res) => {
+    const { bankId } = req.params
 
-  try {
-    const bank = await Bank.deleteOne({
-      _id: bankId,
-    })
-    return res.status(200).json({
-      message: "Bank removed",
-      success: true,
-      bank,
-    })
-  } catch (error) {
-    return res.status(401).json({
-      error,
-    })
-  }
+    try {
+        const bank = await Bank.deleteOne({
+            _id: bankId,
+        })
+        return res.status(200).json({
+            message: "Bank removed",
+            success: true,
+            bank,
+        })
+    } catch (error) {
+        return res.status(401).json({
+            error,
+        })
+    }
 })
 
 module.exports = router
--- routes/banks/resolveBankAccount.js
+++ routes/banks/resolveBankAccount.js
@@ -3,12 +3,12 @@ const checkAuth = require("../../middleware/checkAuth")
 
 const Bank = require("../../models/Banks")
 
-router.post("/resolveBank", checkAuth, (req, res) => {
-    const { user, bankId, accountNumber, resolve } = req.body
+router.post("/resolveBank/:user/:bankId/:accountNumber", checkAuth, (req, res) => {
+    const { user, bankId, accountNumber } = req.params
 
-    Bank.updateOne({ $and: [{ bankId }, { accountNumber }, { user }] }, { $set: { resolve } })
+    Bank.updateOne({ $and: [{ bankId }, { accountNumber }, { user }] }, { $set: { resolve: true } })
         .exec()
-        .then((bank) => {
+        .then(bank => {
             Bank.findOne({ $and: [{ user }, { bankId }, { accountNumber }] })
                 .then((bank) => {
                     return res.status(200).json({
@@ -16,8 +16,7 @@ router.post("/resolveBank", checkAuth, (req, res) => {
                         success: true,
                         bank,
                     })
-                })
-                .catch((error) => {
+                }).catch((error) => {
                     return res.status(401).json({
                         error,
                     })
