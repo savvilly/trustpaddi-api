353ef5085e3dd4719f73fcb77cfbf66000379e15
Terry Amagboro <42579208+rukkiecodes@users.noreply.github.com>
2022-06-22 17:48:21 +0000

    add back account

--- routes/banks/addBankAccount.js
+++ routes/banks/addBankAccount.js
@@ -4,40 +4,40 @@ const checkAuth = require("../../middleware/checkAuth")
 
 const Bank = require("../../models/Banks")
 
-router.post("/addBank", checkAuth, async (req, res) => {
-  const { user, bankId, accountNumber, accountName } = req.body
+router.post("/addBank/:user/:bankId/:accountNumber/:accountName", checkAuth, async(req, res) => {
+    const { user, bankId, accountNumber, accountName } = req.params
 
-  try {
-    const findBank = await Bank.findOne({
-      $and: [{ bankId }, { accountNumber }, { accountName }, { user }],
-    })
+    try {
+        const findBank = await Bank.findOne({
+            $and: [{ bankId }, { accountNumber }, { accountName }, { user }],
+        })
 
-    if (findBank) {
-      return res.status(200).json({
-        findBank,
-        success: false,
-        message: "Bank details already exists",
-      })
-    } else {
-      const newBank = Bank.create({
-        _id: new mongoose.Types.ObjectId(),
-        user,
-        bankId,
-        accountNumber,
-        accountName,
-      })
+        if (findBank) {
+            return res.status(200).json({
+                findBank,
+                success: false,
+                message: "Bank details already exists",
+            })
+        } else {
+            const newBank = Bank.create({
+                _id: new mongoose.Types.ObjectId(),
+                user,
+                bankId,
+                accountNumber,
+                accountName,
+            })
 
-      return res.status(201).json({
-        message: "Bank added successfully",
-        newBank,
-        success: true,
-      })
+            return res.status(201).json({
+                message: "Bank added successfully",
+                newBank,
+                success: true,
+            })
+        }
+    } catch (error) {
+        return res.status(401).json({
+            error,
+        })
     }
-  } catch (error) {
-    return res.status(401).json({
-      error,
-    })
-  }
 })
 
 module.exports = router
