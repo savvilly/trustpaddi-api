f61b8407082d1c43195d760cd7515a87f9afe300
Joshua Paul <cybernerdie@Joshuas-MacBook-Pro.local>
2023-03-08 04:54:35 +0100

    Update Wallet Model and Created WalletTransaction Model

--- src/models/Wallet.ts
+++ src/models/Wallet.ts
@@ -1,24 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { CreateWalletProps } from '../types/wallet';
 
-const walletSchema = new mongoose.Schema<CreateWalletProps>(
+const walletSchema = new Schema<CreateWalletProps>(
   {
     withdrawableAmount: {
       type: Number,
       required: true,
+      min: 0,
     },
 
     amountInTrust: {
       type: Number,
       required: true,
+      min: 0,
     },
-    
+
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
+      index: true,
     },
   },
   { timestamps: true },
 );
+
+walletSchema.virtual('totalAmount').get(function () {
+  return this.withdrawableAmount + this.amountInTrust;
+});
 
 export default mongoose.model('Wallet', walletSchema);
